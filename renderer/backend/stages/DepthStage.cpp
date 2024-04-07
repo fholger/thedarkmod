a134068db374cba747e0cf22c8a4c28e61a51bd0
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2024-04-07 10:52:17 +0000

    Pass model matrix instead of inverse view matrix to depth shader.
    The model matrix is necessary to take clip plane of mirrored subview into account.
    While it is true that modelMatrix = (viewMatrix^-1 * modelViewMatrix), using the approach with inverse matrix is weird.
    
    Performance-wise, the difference is:
      modelMatrix: pass 1 more matrix per surface in uniform
      inverseView: multiply by 1 more matrix per vertex in vertex shader
    I don't think it makes a big difference, so better use simpler appraoch.
    
    TODO: perhaps put clip distance computation under bool condition?
    
    git-svn-id: https://svn.thedarkmod.com/svn/darkmod_src/trunk@10699 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- renderer/backend/stages/DepthStage.cpp
+++ renderer/backend/stages/DepthStage.cpp
@@ -30,7 +30,7 @@ struct DepthStage::DepthUniforms : GLSLUniformGroup {
 	DEFINE_UNIFORM( mat4, projectionMatrix )
 	DEFINE_UNIFORM( mat4, textureMatrix )
 	DEFINE_UNIFORM( vec4, clipPlane )
-	DEFINE_UNIFORM( mat4, inverseView )
+	DEFINE_UNIFORM( mat4, modelMatrix )
 	DEFINE_UNIFORM( sampler, texture )
 	DEFINE_UNIFORM( float, alphaTest )
 	DEFINE_UNIFORM( vec4, color )
@@ -80,10 +80,6 @@ void DepthStage::DrawDepth( const viewDef_t *viewDef, drawSurf_t **drawSurfs, in
 	uniforms = shader->GetUniformGroup<DepthUniforms>();
 
 	uniforms->projectionMatrix.Set( viewDef->projectionMatrix );
-
-	idMat4 inverseView = idMat4::FromGL( viewDef->worldSpace.modelViewMatrix );
-	inverseView.InverseSelf();
-	uniforms->inverseView.Set( inverseView );
 
 	// pass mirror clip plane details to vertex shader if needed
 	if ( viewDef->clipPlane) {
@@ -233,6 +229,7 @@ void DepthStage::IssueDrawCommand( const drawSurf_t *surf, const shaderStage_t *
 	}
 
 	uniforms->modelViewMatrix.Set( surf->space->modelViewMatrix );
+	uniforms->modelMatrix.Set( surf->space->modelMatrix );
 
 	// TODO #6349: Fix FB_ApplyScissor and use it here
 	uint32_t scissor[4];
