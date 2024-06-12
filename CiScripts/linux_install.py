6380d03c5c64e9d71ccca5b8892188b11559adec
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2024-06-12 21:20:10 +0000

    #6253. Try to install 32-bit libs too.
    git-svn-id: https://svn.thedarkmod.com/svn/darkmod_src/trunk@10778 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- CiScripts/linux_install.py
+++ CiScripts/linux_install.py
@@ -7,4 +7,11 @@ def execute(cmd):
 
 sysname = platform.system().lower()
 if 'windows' not in sysname:
-    os.system('sudo apt-get update && sudo apt-get -y install mesa-common-dev libglu1-mesa-dev xorg-dev libxcb-*-dev libx11-xcb-dev libxxf86vm-dev libxext-dev')
+    # this is enough for 64 bit 
+    os.system('sudo apt-get update')
+    os.system('sudo apt-get -y install mesa-common-dev libglu1-mesa-dev xorg-dev libxcb-*-dev libx11-xcb-dev libxxf86vm-dev libxext-dev')
+    # for 32 bit
+    os.system('sudo dpkg --add-architecture i386')
+    os.system('sudo apt-get update')
+    os.system('sudo apt-get -y install gcc-multilib g++-multilib')
+    os.system('sudo apt-get -y install mesa-common-dev:i386 libglu1-mesa-dev:i386 xorg-dev:i386 libxcb-*-dev:i386 libx11-xcb-dev:i386 libxxf86vm-dev:i386 libxext-dev:i386')
